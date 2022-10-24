6d6db849d0d0f6141d14ff6ba7345823faa81615
orico83 <orico8305@gmail.com>
2022-10-24 10:33:12 +0300

    start

--- client.py
+++ client.py
@@ -1,7 +1,24 @@
+import hashlib
 import socket
 
 IP = '127.0.0.1'
 PORT = 8080
+MD5_STR = "81dc9bdb52d04dc20036dbd8313ed055"
+"""my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.connect((IP, PORT))"""
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_socket.connect((IP, PORT))
+
+def calculate_range(start, end, md5_str):
+    while start != end:
+        if hashlib.md5(str(start).encode()).hexdigest() == md5_str:
+            return str(start)
+        start += 1
+    return "NOT FOUND"
+
+
+def main():
+    print(calculate_range(0, 2000, MD5_STR))
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -3,9 +3,9 @@ import socket
 IP = '0.0.0.0'
 PORT = 8080
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+"""server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((IP, PORT))
-server_socket.listen()
+server_socket.listen()"""
 
 
 def handle_connection():
