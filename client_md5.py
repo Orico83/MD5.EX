bc5906272c324f52f78f696c6140270f5cdd53c8
Ori <orico8305@gmail.com>
2022-11-07 20:13:32 +0200

    new

--- client_md5.py
+++ client_md5.py
@@ -1,14 +1,20 @@
 import hashlib
 import socket
 from os import cpu_count
+from threading import Thread
 
 SERVER_IP = '127.0.0.1'
 PORT = 8080
 MAX_PACKET = 1024
-CHUNK_SIZE = 99999999
+CHUNK_SIZE = 2000000
 ORIGINAL_LEN = 7
-NOT_FOUND = "NOT FOUND"
-
+answer = "NOT FOUND"
+free_cpus = cpu_count()
+total_cpu = cpu_count()
+threads = []
+starts = 0
+mid = 0
+ends = 0
 
 """def divide_md5(md5_str, start, end):
     found = False
@@ -22,28 +28,84 @@ NOT_FOUND = "NOT FOUND"
     return check_pass"""
 
 
-def calculate_password(md5_str, start, end):
-    while start != end:
-        if hashlib.md5(str(start).zfill(ORIGINAL_LEN).encode()).hexdigest() == md5_str:
-            return str(start)
-        start += 1
-    return NOT_FOUND
+def give_range(start, end):
+    """
+    gives range to the client threads.
+    :param start: int
+    :param end: int
+    :return: start and mid
+    """
+    global starts
+    global ends
+    starts = start
+    ends = starts + CHUNK_SIZE
+    return starts, ends
+
+
+def calculate_password(md5_str, strt, end):
+    global answer
+    while strt != end:
+        if hashlib.md5(str(strt).zfill(ORIGINAL_LEN).encode()).hexdigest() == md5_str:
+            answer = str(strt)
+        strt += 1
+    print(answer)
 
 
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((SERVER_IP, PORT))
     print("Connected to server")
-    client_socket.send(str(cpu_count()).encode())
-    print("Sent CPU count")
-    MD5_STR = client_socket.recv(MAX_PACKET).decode()
-    print("received hash: " + MD5_STR)
-    start = int(client_socket.recv(MAX_PACKET).decode())
-    print("start: " + str(start))
-    end = int(client_socket.recv(MAX_PACKET).decode())
-    print("end: " + str(end))
-    client_socket.send(calculate_password(MD5_STR, start, end).encode())
-    print(calculate_password(MD5_STR, start, end))
+    try:
+        while True:
+            client_socket.send(str(cpu_count()).encode())
+            print("Sent CPU count")
+            data = client_socket.recv(1024).decode()
+            print("The server sent " + data)
+            if "FOUND" not in data:
+                global free_cpus
+                global total_cpu
+                global starts
+                global mid
+                global ends
+                global plus
+                while True:
+                    msg = data.split("$")[0]
+                    start = int(data.split("$")[1])
+                    end = int(data.split("$")[2])
+                    while free_cpus > 0:
+                        lis = give_range(start, end)
+                        st = lis[0]
+                        en = lis[1]
+                        thread = Thread(target=calculate_password, args=(msg, st, en))
+                        free_cpus -= 1
+                        thread.start()
+                        threads.append(thread)
+                        print(f"starting thread number {total_cpu - free_cpus}..")
+                        client_socket.send(answer.encode())
+                        starts = 0
+                        mid = 0
+                        ends = 0
+                        plus = 1 + starts
+                        break
+                    if free_cpus == 0:
+                        print("waits for the threads to end..")
+                        for thread in threads:
+                            thread.join()
+                        free_cpus = total_cpu
+                    break
+                break
+            else:
+                print("found the message!\n disconnecting...")
+                client_socket.close()
+                exit()
+    except socket.error as err:
+        print(str(err))
+        print("disconnecting...")
+        client_socket.close()
+        exit()
+    finally:
+        client_socket.close()
+        exit()
 
 
 if __name__ == "__main__":
--- server_md5.py
+++ server_md5.py
@@ -1,6 +1,5 @@
 import socket
 from threading import Thread
-from thread_return import ThreadReturn
 
 MD5_STR = "fcea920f7412b5da7be0cf42b8c93759"
 MAX_NUM = 9999999999
@@ -11,49 +10,64 @@ QUEUE_LEN = 10
 CHUNK_SIZE = 100000
 NUM_THREADS = 4
 answers_list = []
+threads = []
 end = 0
 start = 0
+cpu_count = 0
+answer = "NOT FOUND"
+found = False
 
-#def create_msg()
 
-
-def handle_connection(client_socket, client_address, start):
+def create_msg():
+    global start
     global end
-    cpu_count = int(client_socket.recv(MAX_PACKET).decode())
-    print(cpu_count)
-    end = start + CHUNK_SIZE * cpu_count
-    client_socket.send(MD5_STR.encode())
-    client_socket.send(str(start).encode())
-    client_socket.send(str(end).encode())
-    answer = client_socket.recv(MAX_PACKET).decode()
-    print(answer)
-    answers_list.append(answer + str(end))
+    end += CHUNK_SIZE * cpu_count
+    msg = MD5_STR + "$" + str(start) + "$" + str(end)
+    start = end
+    return msg
+
+
+def handle_connection(client_socket):
+    global found, end, cpu_count, answer
+    try:
+        while True:
+            while not found:
+                cpu_count = int(client_socket.recv(MAX_PACKET).decode())
+                print("Client CPU count: " + str(cpu_count))
+                client_socket.send(create_msg().encode())
+                answer = client_socket.recv(MAX_PACKET).decode()
+                print(answer)
+                if answer != "NOT FOUND":
+                    found = True
+            break
+    except socket.error as err:
+        print(str(err))
+    finally:
+        client_socket.close()
 
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    threads = []
-    answer = -1
-    global start
-    try:
-        server_socket.bind((IP, PORT))
-        server_socket.listen(QUEUE_LEN)
-        print("Listening for connections on port %d" % PORT)
-        while True:
-            client_socket, client_address = server_socket.accept()
-            for i in range(NUM_THREADS):
-                thread = ThreadReturn(target=handle_connection, args=(client_socket, client_address, start))
+    server_socket.bind((IP, PORT))
+    server_socket.listen(QUEUE_LEN)
+    print("Listening for connections on port %d" % PORT)
+    global found
+    while True:
+        try:
+            while not found:
+                client_socket, client_address = server_socket.accept()
+                print(f"connected to client {client_address}")
+                thread = Thread(target=handle_connection, args=(client_socket,))
                 thread.start()
                 threads.append(thread)
-                answer += 1
-            for thread in threads:
-                thread.join()
-                start = end
-                print(start)
-    except socket.error as err:
-        print("received socket exception -" + str(err))
-    finally:
-        server_socket.close()
+            if found:
+                for thread in threads:
+                    thread.join()
+            break
+        except socket.error as err:
+            print("received socket exception -" + str(err))
+        finally:
+            server_socket.close()
 
 
 if __name__ == "__main__":
